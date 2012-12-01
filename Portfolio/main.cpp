a0021b15e7721d31762690ceacf4fe2831e906d4
Victor Birath <victor.birath@gmail.com>
2012-12-01 20:37:20 +0100

    Initial Commit

---
+++ Portfolio/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Portfolio
+//
+//  Created by Victor Birath on 2012-12-01.
+//  Copyright (c) 2012 database. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
